b73e5ccd4b2ec8dd81d7eef35d914945121e127c
rectina <610431650@qq.com>
2018-08-24 19:44:50 +0800

    remove ctrf

--- learning git.py
+++ learning git.py
@@ -1,13 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
-
-
 https://www.liaoxuefeng.com/wiki/0013739516305929606dd18361248578c67b8067c8c017b000
-
-
-# In[ ]:
 
 
 http://www.runoob.com/w3cnote/git-guide.html
